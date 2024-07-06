e7a86010f3de20c915a169d3d45b915792f8cf7e
kapil hemne <hemnekapil24@gmail.com>
2024-07-06 17:21:38 +0530

    displayGenres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -28,7 +28,7 @@ const GameGrid = () => {
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
       padding="10px"
-      spacing={6}
+      spacing={3}
     >
       {loading &&
         skeltons.map((id) => (
--- src/hooks/useGeneres.ts
+++ src/hooks/useGeneres.ts
@@ -1,50 +1,11 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-clients";
-import { CanceledError } from "axios";
+import useData from "./usedata";
 
-interface Genre {
+export interface Genre {
   id: number;
   name: string;
+  image_background: string;
 }
-interface FetchgenresResponse {
-  count: number;
-  results: Genre[];
-}
-const useGeneres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
+const useGenres = () => useData<Genre>("/genres");
 
-    apiClient
-      .get<FetchgenresResponse>("/games", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) {
-          console.log("Request canceled", err.message);
-        } else if (err.response) {
-          setError(`Error: ${err.response.status} - ${err.response.data}`);
-        } else if (err.request) {
-          setError("Network error: No response received from server.");
-        } else {
-          setError(`Error: ${err.message}`);
-        }
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-
-    return () => {
-      controller.abort();
-    };
-  }, []);
-  return { genres, error, loading };
-};
-
-export default useGeneres;
+export default useGenres;
