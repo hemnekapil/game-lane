95e7f70cbabf2bddf482a5043aee3c23517543a7
kapil hemne <hemnekapil24@gmail.com>
2024-07-12 21:52:33 +0530

    Gamequery simplify

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,16 +4,19 @@ import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
-  selectedPlatform: Platform | null;
+  selectedPlatformId?: number;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
   const { data, error } = usePlatforms();
+  const selectedPlatforms = data?.results.find(
+    (p) => p.id === selectedPlatformId
+  );
   if (error) return null;
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || "Platforms"}
+        {selectedPlatforms?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data?.results.map((platform) => (
--- src/hooks/userGames.ts
+++ src/hooks/userGames.ts
@@ -21,8 +21,8 @@ const userGames = (gameQuery: GameQuery) =>
     queryFn: ({ pageParam = 1 }) => {
       return apiClient.getAll({
         params: {
-          genres: gameQuery.genre?.id,
-          parent_platforms: gameQuery.platform?.id,
+          genres: gameQuery.genreId,
+          parent_platforms: gameQuery.platformId,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
           page: pageParam,
